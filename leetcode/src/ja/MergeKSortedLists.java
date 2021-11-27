0f7f40a474c7000fdf407a8c911f80c141452ee6
kristya-art <shiriagina@yahoo.com>
2021-11-27 22:53:53 +0100

    added solution for merge k Sorted Lists

--- leetcode/src/ja/MergeKSortedLists.java
+++ leetcode/src/ja/MergeKSortedLists.java
@@ -1,77 +1,77 @@
 package ja;
-
-import org.w3c.dom.NodeList;
-
-import javax.swing.*;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Stream;
-
 /**
-You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+ You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
  Merge all the linked-lists into one sorted linked-list and return it.
  */
-/*
-algorithm will go throw each element in array and will always compare the first element of each array with each
-other. Each time when the linkedlist has a min number the pointer will be moved to the next element. When the
-array will have all elements empty a program will be terminated.
- */
-public class MergeKSortedLists {
-    public static class ListNode extends MergeKSortedListsTest.ListNode {
-        int val;
-        ListNode next;
-        ListNode() {}
-        ListNode(int val){this.val=val;}
-        ListNode(int val, ListNode next){this.val=val;this.next=next;}
+import java.util.ArrayList;
+import java.util.List;
 
+public class MergeKSortedLists extends ListNode {
+
+    public static int getLength(ListNode[] lists) {
+
+        return lists.length;
     }
-//    public ListNode mergeKLists(ListNode[] lists){
-//        ListNode n  = new ListNode();
-//        int min = 0;
-//        for(int i =0;i <lists.length;i++){
-//            min = lists[i].val;
-//
-//        }
-//    }
-public static List<Integer> test(ListNode[] lists){
-        int count=0;
-        int min = lists[0].val;
-        ListNode output = new ListNode();
-        ListNode temp = new ListNode();
-        temp = output;
 
-        List result = new ArrayList();
-        for(int j=0;j<3;j++){
-   for(int i = 0;i<lists.length;i++) {
-       if (lists[i].val < min) {
-           min = lists[i].val;
-           count = i;
-       }
-   }
-            result.add(min);
-            lists[count] = lists[count].next;
-
-   }
+    public static ListNode mergeKLists(ListNode[] lists) {
+        List<Integer> output = new ArrayList<>();
+        int min = 0;
+        int indexFormin = 0;
+        int end = 0;
+        int count = indexFormin;
+        int l = lists.length;
 
 
-//        output.val=min;
-//
-//        ListNode prossimo = new ListNode();
-//        output.next = prossimo;
-//
+        while (end != lists.length) {
+             if(l==1 && isNull(lists[0])){break;}
+          //  if(l == 1){output.add(lists[0].val);break;}
+             else if(isNull(lists[indexFormin])&& indexFormin==l-1) {
+                 break;
+             }
+            if (isNull(lists[indexFormin])&&indexFormin<l) {
+                indexFormin = indexFormin + 1;
+                continue;
+            } else {
+                min = lists[indexFormin].val;
+            }
 
-   return result;
-}
-    public static void printLinkedList(ListNode listNode){
-        System.out.println("[");
-        while(listNode!=null){
-            System.out.println(listNode.val);
-            listNode = listNode.next;
+            for (int i = indexFormin; i < lists.length; i++) {
+                if (!isNull(lists[i]) && min >= lists[i].val) {
+                    min = lists[i].val;
+                    count = i;
+                }
+            }
+            output.add(min);
+
+            if (!isNull(lists[count])) {
+                lists[count] = lists[count].next;
+            }
+            if (isNull(lists[count])) {
+                end += 1;
+            }
         }
-        System.out.println("]");
+System.out.println(output + "this is output");
+        int j = 1;
+        int arrayLength = output.size();
+        if(arrayLength==0){return null;}
+        ListNode head = new ListNode();
+        if (arrayLength > 0) {
+            head.val = output.get(0);
+
+        ListNode temp = head;
+        while (j < arrayLength) {
+            ListNode temptemp = new ListNode();
+            temp.next = temptemp;
+            temp = temptemp;
+            temp.val = output.get(j);
+            j++;
+
+        }
     }
-public static void main (String args[]){
+           return head;
+    }
+
+    public static void main(String args[]) {
         ListNode l1 = new ListNode(1);
         ListNode l11 = new ListNode(3);
         ListNode l12 = new ListNode(5);
@@ -84,22 +84,34 @@ public static void main (String args[]){
         ListNode l22 = new ListNode(6);
 
         l2.next = l21;
-        l21.next= l22;
+        l21.next = l22;
 
         //////
         ListNode l3 = new ListNode(4);
-        ListNode l31 = new ListNode(3);
-        ListNode l32 = new ListNode(7);
-
-        l3.next = l31;
-        l31.next = l32;
-
-        ListNode[] lists = new ListNode[]{l1,l2,l3};
-
-        System.out.println(test(lists));
 
 
+        ListNode[] lists = new ListNode[]{l1, l2, l3};
+
+        printLinkedList(mergeKLists(lists));
+
+        ////////check for empty list
+        ListNode l4 = new ListNode();
+        l4.val=1;
+
+       ListNode[] lists2 = new ListNode[]{};
+
+      // printLinkedList(mergeKLists(lists2));
+       ///
+        ////////check for empty list
+        ListNode l5 = new ListNode();
+        ListNode l6 = new ListNode();
 
 
-}
+        ListNode[] lists3 = new ListNode[]{l5,l6};
+
+        printLinkedList(mergeKLists(lists3));
+
+
+
+    }
 }
